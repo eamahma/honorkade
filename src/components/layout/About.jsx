8ec0bc8bbf7fc547c04ea13a8bed850abf4fce1b
Amir Ahmadi <eamahma@gmail.com>
2022-01-08 16:36:51 -0500

    about page updated

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -2,23 +2,28 @@ import React from "react";
 
 function About() {
   return (
+    <div class="flex">
     <div class="card card-bordered">
       <figure>
-        <img src="https://picsum.photos/id/1005/400/250" alt="myself"/>
+        <img src="/assets/butterfly-xsm.jpg" alt="myself" class="h-80"/>
       </figure>
       <div class="card-body">
         <h2 class="card-title">Mitra Soltani</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-          neque eos veritatis modi possimus maiores temporibus nostrum beatae
-          atque tenetur eaque molestiae sed consequatur quis. In quam iusto
-          aliquid ipsum.
+          The beginning of my artistic career goes back to my early childhood. As a kid, I was always fascinated with the beauty and aesthetics of the world around me. My artworks were always praised by my family and teachers which invoked my passion for creating art and lead me to pursue a career as an artist. 
         </p>
-        <div class="justify-end card-actions">
-          <button class="btn btn-secondary">More info</button>
-        </div>
+        <p>
+          I graduated with a (bachelor-master?) degree in graphic orientation art from the University of Tehran (?) in Iran. And started my practice as a graphic designer, working on illustrations and sculptures. However, my love for painting from my early years remained and it brought me to start painting again as a side project. 
+        </p>
+        <p> 
+          Over the years my fondness for painting grew, and the side projects turned into a full-time career. Today, I have devoted all my days to discovering and teaching myself new techniques and working with different mediums. Experience and the love for my work inspire me to create reflections of nature and the people around me in realistic portraits and exotic and sometimes soft landscapes.
+        </p>
+        <p>
+          I am now living in Michigan, US with my family. Their support and appreciation for my work allow me to enjoy and continue making art that has brought me inexpressible joy.
+        </p>
       </div>
     </div>
+    </div> 
   );
 }
 
